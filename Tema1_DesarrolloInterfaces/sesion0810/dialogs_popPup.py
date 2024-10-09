2ed47cb943b94d32870e829db18c48f08a0258b0
OllisDev <ikermagro@outlook.es>
2024-10-09 15:11:43 +0200

    sesion0910

---
+++ Tema1_DesarrolloInterfaces/sesion0810/dialogs_popPup.py
@@ -0,0 +1,39 @@
+
+import sys
+
+from PyQt6.QtWidgets import QApplication, QDialog, QMainWindow, QPushButton, QVBoxLayout, QLabel
+
+class CustomDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("HELLO!")
+
+        layout = QVBoxLayout()
+        message = QLabel("Something happened, is that OK?")
+        layout.addWidget(message)
+        layout.addWidget(self.buttonBox)
+        self.setLayout(layout)
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        button = QPushButton("Press me for a dialog!")
+        button.clicked.connect(self.button_clicked)
+        self.setCentralWidget(button)
+
+    def button_clicked(self, s):
+        print("click", s)
+
+        dlg = QDialog(self)
+        dlg.setWindowTitle("HELLO!")
+        dlg.exec()
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
