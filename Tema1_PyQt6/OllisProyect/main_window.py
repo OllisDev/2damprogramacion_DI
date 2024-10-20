89a8970748d3affc098fe02dbb2af7aab7a8084d
OllisDev <ikermagro@outlook.es>
2024-10-20 13:15:08 +0200

    sesion2010

--- Tema1_PyQt6/OllisProyect/main_window.py
+++ Tema1_PyQt6/OllisProyect/main_window.py
@@ -6,6 +6,25 @@ class main_window(QMainWindow):
     def __init__(self):
         super(main_window, self).__init__()
         self.setWindowTitle("OllisProject")
+
+        self.traduccion = { 
+            "Español": {
+                "title": "¡BIENVENIDO!",
+                "register": "Regístrate",
+                "login": "Inicie sesión",
+                "language": "Idioma",
+                "exit": "Salir"
+            },
+            "English": {
+                "title": "WELCOME!",
+                "register": "Register",
+                "login": "Log in",
+                "language": "Language",
+                "exit": "Exit"
+            }
+        }
+
+        self.current_language = "Español"
         
         layout_main = QVBoxLayout()
         layout_title = QVBoxLayout()
@@ -13,45 +32,47 @@ class main_window(QMainWindow):
         layout_btn_exit = QHBoxLayout()
         layout_lenguage = QHBoxLayout()
 
-        title = QLabel("¡BIENVENIDO!")
-        title.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        fuente = title.font()
+        self.title = QLabel("¡BIENVENIDO!")
+        self.title.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        fuente = self.title.font()
         fuente.setPointSize(20)
-        title.setFont(fuente)
-        layout_title.addWidget(title)
+        self.title.setFont(fuente)
+        layout_title.addWidget(self.title)
         layout_title.setAlignment(Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignHCenter)
 
         spacer_top = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
         spacer_bottom = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
         layout_btn_register_login.addItem(spacer_top)
 
-        btn_register = QPushButton("Regístrate")
-        btn_register.setFixedSize(100, 30)
-        layout_btn_register_login.addWidget(btn_register)
-        layout_btn_register_login.setAlignment(btn_register, Qt.AlignmentFlag.AlignHCenter)
+        self.btn_register = QPushButton(self.traduccion[self.current_language]["register"])
+        self.btn_register.setFixedSize(100, 30)
+        layout_btn_register_login.addWidget(self.btn_register)
+        layout_btn_register_login.setAlignment(self.btn_register, Qt.AlignmentFlag.AlignHCenter)
         
         
-        btn_login = QPushButton("Inicie sesión")
-        btn_login.setFixedSize(100,30)
-        layout_btn_register_login.addWidget(btn_login)
-        layout_btn_register_login.setAlignment(btn_login, Qt.AlignmentFlag.AlignHCenter)
+        self.btn_login = QPushButton(self.traduccion[self.current_language]["login"])
+        self.btn_login.setFixedSize(100,30)
+        layout_btn_register_login.addWidget(self.btn_login)
+        layout_btn_register_login.setAlignment(self.btn_login, Qt.AlignmentFlag.AlignHCenter)
 
         layout_btn_register_login.addItem(spacer_bottom) 
         
 
-        txt_lenguage = QLabel("Idioma")
-        layout_lenguage.addWidget(txt_lenguage)
+        self.txt_lenguage = QLabel(self.traduccion[self.current_language]["language"])
+        layout_lenguage.addWidget(self.txt_lenguage)
 
-        lenguage = QComboBox()
-        lenguage.addItems(["Español", "English"])
-        layout_lenguage.addWidget(lenguage)
+        self.lenguage = QComboBox()
+        self.lenguage.addItems(["Español", "English"])
+        self.lenguage.currentIndexChanged.connect(self.cambiar_idioma)
+        layout_lenguage.addWidget(self.lenguage)
         
         
         spacer_exit = QSpacerItem(30, 60, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
         layout_btn_exit.addItem(spacer_exit)
 
-        btn_exit = QPushButton("Salir")
-        layout_btn_exit.addWidget(btn_exit)
+        self.btn_exit = QPushButton(self.traduccion[self.current_language]["exit"])
+        self.btn_exit.clicked.connect(self.close)
+        layout_btn_exit.addWidget(self.btn_exit)
 
         layout_main.addLayout(layout_title)
         layout_main.addLayout(layout_btn_register_login)
@@ -66,8 +87,16 @@ class main_window(QMainWindow):
         widget_main.setLayout(layout_main)
         self.setCentralWidget(widget_main)
 
-        
-         
+    def cambiar_idioma(self):
+        self.current_language = self.lenguage.currentText()
+
+        self.title.setText(self.traduccion[self.current_language]["title"])
+        self.btn_register.setText(self.traduccion[self.current_language]["register"])
+        self.btn_login.setText(self.traduccion[self.current_language]["login"])
+        self.txt_lenguage.setText(self.traduccion[self.current_language]["language"])
+        self.btn_exit.setText(self.traduccion[self.current_language]["exit"])
+
+
 
 if __name__ == "__main__":
     
